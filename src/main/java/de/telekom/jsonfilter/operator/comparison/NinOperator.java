83c6b03dc05d8738c792b55241c5e9202f9f099b
Jens Fischer <jens-fischer@telekom.de>
2025-01-17 07:35:16 +0100

    Add license header to NinOperator

--- src/main/java/de/telekom/jsonfilter/operator/comparison/NinOperator.java
+++ src/main/java/de/telekom/jsonfilter/operator/comparison/NinOperator.java
@@ -1,3 +1,7 @@
+// Copyright 2025 Deutsche Telekom IT GmbH
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package de.telekom.jsonfilter.operator.comparison;
 
 import de.telekom.jsonfilter.operator.EvaluationResult;
